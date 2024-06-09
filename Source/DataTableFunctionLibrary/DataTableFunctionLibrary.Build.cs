5671ba79528112a5d2fadf0da1420c8d2b72249a
rtkz <154375853+rtkzboss@users.noreply.github.com>
2024-06-09 18:15:03 -0500

    Simplify module rules

--- Source/DataTableFunctionLibrary/DataTableFunctionLibrary.Build.cs
+++ Source/DataTableFunctionLibrary/DataTableFunctionLibrary.Build.cs
@@ -2,10 +2,6 @@ using UnrealBuildTool;
 
 public class DataTableFunctionLibrary : ModuleRules {
     public DataTableFunctionLibrary(ReadOnlyTargetRules Target) : base(Target) {
-        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-        bLegacyPublicIncludePaths = false;
-        ShadowVariableWarningLevel = WarningLevel.Warning;
-        
         PublicDependencyModuleNames.AddRange(new string[] {
             "Core",
             "CoreUObject",
